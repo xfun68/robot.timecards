3b129f6cbaa326b008b022bedd2f7cf62bb439b3
summer <xiasiyu@gmail.com>
2014-07-24 13:29:12 +0800

    comment on new and dismission

--- lib/time_card_hours_parser.rb
+++ lib/time_card_hours_parser.rb
@@ -12,7 +12,8 @@ class TimeCardHoursParser
     title = table.xpath('tr/th/text()')
     @weeks = title.map { |t| t.content }.select { |t| Date.strptime(t, "%Y-%m-%d") rescue false }
     data = table.xpath('tr')
-    @records = data.map { |row| parse_record(row) }.compact.select { |rec| !rec[:illegal_hours_weeks].empty? && !rec[:is_new] && !rec[:is_dismiss] }
+    #@records = data.map { |row| parse_record(row) }.compact.select { |rec| !rec[:illegal_hours_weeks].empty? && !rec[:is_new] && !rec[:is_dismiss] }
+    @records = data.map { |row| parse_record(row) }.compact.select { |rec| !rec[:illegal_hours_weeks].empty? }
   end
 
   private
@@ -25,11 +26,11 @@ class TimeCardHoursParser
     record[:office] = row.at_xpath("td[1]/a/text()").content.to_s
     (0...@weeks.length).each do |i|
       hours = row.at_xpath("td[#{3+i}]/table/tr/td/text()").content.to_f
-      start_time = row.at_xpath("td[#{3+@weeks.length}]/table/tr/td/text()").content.to_s
-      end_time = row.at_xpath("td[#{4+@weeks.length}]/table/tr/td/text()").content.to_s
+      #start_time = row.at_xpath("td[#{3+@weeks.length}]/table/tr/td/text()").content.to_s
+      #end_time = row.at_xpath("td[#{4+@weeks.length}]/table/tr/td/text()").content.to_s
       record[:illegal_hours_weeks] = record[:illegal_hours_weeks].merge({@weeks[i] => hours}) if hours < 40.0 && hours > 0
-      record[:is_new] = employed_in_week(start_time, @weeks[i])
-      record[:is_dismiss] = !end_time.blank? && Date.parse(end_time) < Date.parse(@weeks[i]).prev_day(2)
+      #record[:is_new] = employed_in_week(start_time, @weeks[i])
+      #record[:is_dismiss] = !end_time.blank? && Date.parse(end_time) < Date.parse(@weeks[i]).prev_day(2)
     end
     record
   end
