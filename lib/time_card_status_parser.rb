ecf8265bd0729e10b0cd19bed56f0f7df0f7ec3e
twflu <yzzhou@thoughtworks.com>
2014-08-05 14:53:39 +0800

    [#N/A][yzzhou & tychio] - fix defect:the report is empty when all offices members have filled timecard properly.

--- lib/time_card_status_parser.rb
+++ lib/time_card_status_parser.rb
@@ -4,12 +4,26 @@ require_relative './mail_actor'
 class TimeCardStatusParser
   def self.parse(mail)
     doc = Nokogiri::HTML(mail.body.decoded)
+    if result = mail.body.match(/Resource: Region.*<span  class="filterValue">(.*)<\/span>/)
+      regions = result.captures
+    end
+
     table=doc.css('table.reportTable.tabularReportTable')
-    cities = table.xpath('tr/td/span/a/text()').map {|cell| cell.content }
+
+    result = {}
+    cities = regions[0].to_s.split(',').map do |city|
+      city.to_s.capitalize.gsub("&#39;", "'")
+    end
+
+    cities.each do |city|
+      result[city] = 0
+    end
+
+    missingTimeCardCities = table.xpath('tr/td/span/a/text()').map {|cell| cell.content }
+
     numbers = table.xpath('tr/td/strong/span/text()').map {|cell| cell.content }.map {|text| text.scan(/\((\d+)/).flatten.first.to_i}
     numbers.shift # the first number is total count, discard it
-    result = {}
-    cities.each_index { |i| result[cities[i]] = numbers[i] }
+    missingTimeCardCities.each_index { |i| result[missingTimeCardCities[i]] = numbers[i] }
     result
   end
 end
